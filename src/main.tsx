be4461120f576635d1d68bab1eafa2b230547e37
Ben <benyamin.ak@hotmail.com>
2026-01-03 21:20:26 +0100

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { ApiReferenceReact } from '@scalar/api-reference-react'
+import '@scalar/api-reference-react/style.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <ApiReferenceReact
+        configuration={{
+          // url: 'http://0.0.0.0:8000/v1/auth/doc.json',
+          url: 'https://fnapi.llamatracker.net/v1/auth/doc.json',
+        }}
+      />
+    </StrictMode>,
 )
